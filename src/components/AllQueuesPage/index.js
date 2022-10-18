db246e5b44de07bd9aef862c48c5333a0ac29404
borroxfinance <104905491+borroxfinance@users.noreply.github.com>
2022-10-18 10:02:02 +0200

    Update index.js

--- src/components/AllQueuesPage/index.js
+++ src/components/AllQueuesPage/index.js
@@ -58,6 +58,7 @@ const getSuccessRatio = ()=>{
 }
 useEffect(()=>{
     const apiUrl = process.env.API_URL
+    console.log(`Making request to ${apiUrl}api/getAllQueues`)
     const fetchData = async () => {
          const resp = await fetch(`${apiUrl}api/getAllQueues`)
          const respBody = await resp.json()
